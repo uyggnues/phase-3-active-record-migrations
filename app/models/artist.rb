ce96c1d68471ad0ec8f9b9c713f3d67a51c9ae36
Seunggyu <seunggyul14@icloud.com>
2023-01-12 08:38:32 -0800

    commit tree 13a908a2ceca29c2070b0de909e0598affd77a9f

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,11 @@
 # add the Artist class here
+class Artist < ActiveRecord::Migration[6.1]
+    def change
+        create_table :artists do |t|
+            t.string(:name)
+            t.string(:genre)
+            t.integer(:age)
+            t.string(:hometown)
+          end
+    end
+end
